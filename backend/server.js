f5ce0a0d440abb3aebb31e0f7a9b0589186f4fa8
Kolum <kolumronnie@gmail.com>
2025-06-12 07:51:12 +0300

    Added endpoints to retrieve, update and delete products

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,12 @@ import { connectDB } from './config/db.js';
 import Product from './models/products.model.js';
 
 const app = express();
+
 dotenv.config();
 
-app.post("/products", async(req,res)=>{
+app.use(express.json()); //Allows us to accept json data in req.body
+
+app.post("/api/products", async(req,res)=>{
     const product = req.body;
     if(!product.name || !product.price || !product.image){
         return res.status(400).json({success:false, message:"Please provide all fields"});
@@ -23,6 +26,49 @@ app.post("/products", async(req,res)=>{
     }
 });
 
+app.delete("/api/products/:id", async(req,res)=>{
+    const { id } = req.params;
+
+    try {
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).json({success:false, message:"Product not found"});
+        }
+        res.status(200).json({success:true, data: product});
+    } catch (error) {
+        console.error("Error in delete product:", error.message);
+        res.status(500).json({ success: false, message: "Server Error"});
+    }
+}
+);
+app.get("/api/products", async(req,res)=>{
+    try {
+        const products = await Product.find();
+        res.status(200).json({success:true, data: products});
+    } catch (error) {
+        console.error("Error in get products:", error.message);
+        res.status(500).json({ success: false, message: "Server Error"});
+    }
+});
+app.put("/api/products/:id", async(req,res)=>{
+    const { id } = req.params;
+    const productData = req.body;
+
+    try {
+        const product = await Product.findByIdAndUpdate(id, productData, {
+            new: true,
+            runValidators: true
+        });
+        if(!product){
+            return res.status(404).json({success:false, message:"Product not found"});
+        }
+        res.status(200).json({success:true, data: product});
+    } catch (error) {
+        console.error("Error in update product:", error.message);
+        res.status(500).json({ success: false, message: "Server Error"});
+    }
+}
+);
 app.listen(5000,()=>{
     connectDB();
     console.log('server started at http://localhost:5000');
