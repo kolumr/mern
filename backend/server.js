f4b4a3ca85bf3ecc6263b8cfd13df0ad3eae7da1
Kolum <kolumronnie@gmail.com>
2025-06-12 06:13:50 +0300

    Initialized the DB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv'
+import { connectDB } from './config/db.js';
 
 const app = express();
-
+dotenv.config();
 app.listen(5000,()=>{
+    connectDB();
     console.log('server started at http://localhost:5000');
 });
+
+app.get("/products",(req,res)=>{
+    res.send("Products pulled");
+});
+
